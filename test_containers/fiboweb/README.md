5b5231950eb545e8491dc83f29e08d8c221f1fbf
ssasso <852093+ssasso@users.noreply.github.com>
2020-03-25 08:24:27 +0000

    Load test container

---
+++ test_containers/fiboweb/README.md
@@ -0,0 +1,4 @@
+# Simple Container
+
+Pushed on dockerhub on ssasso/simple:2
+
---
+++ test_containers/fiboweb/server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const express = require('express');
+const os = require("os");
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+function fib(n) {
+    if(n > 1){
+        return fib(n-1) + fib(n-2)
+    } else {
+        return n;
+    }
+}
+
+app.get('/', function(req, res) {
+    res.sendStatus(404);
+});
+
+app.get('/fibonacci/:n', function(req, res) {
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log(`Received FIBONACCI request from: ${ip}`);
+    var nstr = req.params.n || "";
+    var n = parseInt(nstr);
+    if (isNaN(n)) { n = 0; }
+    console.log(`Received input ${n} (${nstr})`);
+    var r = fib(n);
+    console.log(`fib(${n}) = ${r}`);
+    res.send({
+        'n': n,
+        'fib(n)': r
+    });
+});
+
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
